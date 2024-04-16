74138bfa310b6ff77d708a925fbb87a5efd9e980
Komp-880-PC <canek2018mail.ru@gmail.com>
2024-04-16 19:55:29 +0300

    Update repository

---
+++ conftest.py
@@ -0,0 +1,27 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+
+def pytest_addoption(parser):
+    parser.addoption("--browser name", action="store", default="chrome", help="Choose browser: chrome")
+    parser.addoption("--language", action="store",default="en", help="Language")
+
+
+@pytest.fixture(scope="function")
+def browser(request):
+    browser_name = request.config.getoption("browser name")
+    user_language = request.config.getoption("language")
+
+    if browser_name == "chrome":
+        print("\nstart chrome browser for test..")
+        browser = webdriver.Chrome()
+        options = Options()
+        options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+        browser = webdriver.Chrome(options=options)
+    else:
+        raise pytest.UsageError("--browser name should be chrome")
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
+    breakpoint()
